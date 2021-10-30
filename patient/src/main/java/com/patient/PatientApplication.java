0074d257764d6f2215b027c70cbad9071145d814
SohanBappy <sohanbappy@gmail.com>
2021-10-31 03:22:19 +0600

    (JDK-11)  pom, properties updated for jdk-11

--- patient/src/main/java/com/patient/PatientApplication.java
+++ patient/src/main/java/com/patient/PatientApplication.java
@@ -2,11 +2,12 @@ package com.patient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class PatientApplication {
+public class PatientApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PatientApplication.class, args);
