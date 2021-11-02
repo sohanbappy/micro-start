939fb7f78940cc96cfdf00464be66cc211936d75
SohanBappy <sohanbappy@gmail.com>
2021-11-02 23:20:01 +0600

    Hystrix Dashboard added

---
+++ hystrix-dashboad/src/main/java/com/micro/dashboad/HystrixDashboadApplication.java
@@ -0,0 +1,13 @@
+package com.micro.dashboad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixDashboadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboadApplication.class, args);
+	}
+
+}
---
+++ hystrix-dashboad/src/test/java/com/micro/dashboad/HystrixDashboadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.dashboad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
