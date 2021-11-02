d444fba3a4328ddfc9e527885cd5d57619bc168b
SohanBappy <sohanbappy@gmail.com>
2021-11-02 19:37:30 +0600

    FallBack From GateWay removed

--- api-gateway/src/main/java/com/api/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/api/gateway/ApiGatewayApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableHystrix
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
