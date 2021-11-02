63f0dd2774019e0e31d472a8f7a93ef4412dabb6
SohanBappy <sohanbappy@gmail.com>
2021-11-02 19:20:00 +0600

    Hystrix,circuitbreaker Implemented in GateWay (but failed)

--- admission/src/main/java/com/admission/controller/HomeController.java
+++ admission/src/main/java/com/admission/controller/HomeController.java
@@ -26,21 +26,31 @@ public class HomeController {
     }
 
     @RequestMapping("/allDoctors")
-    public List<Doctor> getAllDoctors() throws ExecutionException, InterruptedException {
-        CommonHystrixCommand<List<Doctor>> command = new CommonHystrixCommand<List<Doctor>>("appointment-group",
-                () -> restTemplate.getForObject("http://doctor-service/doctor/all", List.class)
-                , () -> new ArrayList<>());
-        Future<List<Doctor>> docFuture = command.queue();
-        return docFuture.get();
+    public List<Doctor> getAllDoctors() {
+        return restTemplate.getForObject("http://doctor-service/doctor/all", List.class);
     }
 
     @RequestMapping("/getDoctor/{Id}")
-    public Doctor getDoctorById(@PathVariable("Id") int Id) throws ExecutionException, InterruptedException {
-        CommonHystrixCommand<Doctor> command = new CommonHystrixCommand<Doctor>("appointment-group",
-                () -> restTemplate.getForObject("http://doctor-service/doctor/" + Id, Doctor.class)
-                , () -> new Doctor());
-        Future<Doctor> docFuture = command.queue();
-        return docFuture.get();
+    public Doctor getDoctorById(@PathVariable("Id") int Id) {
+        return restTemplate.getForObject("http://doctor-service/doctor/" + Id, Doctor.class);
     }
+
+//    @RequestMapping("/allDoctors")
+//    public List<Doctor> getAllDoctors() throws ExecutionException, InterruptedException {
+//        CommonHystrixCommand<List<Doctor>> command = new CommonHystrixCommand<List<Doctor>>("appointment-group",
+//                () -> restTemplate.getForObject("http://doctor-service/doctor/all", List.class)
+//                , () -> new ArrayList<>());
+//        Future<List<Doctor>> docFuture = command.queue();
+//        return docFuture.get();
+//    }
+//
+//    @RequestMapping("/getDoctor/{Id}")
+//    public Doctor getDoctorById(@PathVariable("Id") int Id) throws ExecutionException, InterruptedException {
+//        CommonHystrixCommand<Doctor> command = new CommonHystrixCommand<Doctor>("appointment-group",
+//                () -> restTemplate.getForObject("http://doctor-service/doctor/" + Id, Doctor.class)
+//                , () -> new Doctor());
+//        Future<Doctor> docFuture = command.queue();
+//        return docFuture.get();
+//    }
 
 }
