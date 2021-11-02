98d60d0e6458ae055111ba3391f87ebcc0bd7383
SohanBappy <sohanbappy@gmail.com>
2021-11-03 00:56:19 +0600

    Configured Dashboard (Hystrix) for Admission

--- hystrix-dashboard/src/main/java/com/micro/dashboad/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/micro/dashboad/HystrixDashboardApplication.java
@@ -2,12 +2,14 @@ package com.micro.dashboad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
-public class HystrixDashboadApplication {
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HystrixDashboadApplication.class, args);
+		SpringApplication.run(HystrixDashboardApplication.class, args);
 	}
 
 }
