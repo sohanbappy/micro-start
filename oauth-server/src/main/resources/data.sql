1109f4e004c0a0171767629737645330a4c4a1bd
SohanBappy <sohanbappy@gmail.com>
2021-11-08 03:33:38 +0600

    Server Db-table created and Dummy-data inserted

---
+++ oauth-server/src/main/resources/data.sql
@@ -0,0 +1,28 @@
+/* password- bappy */
+INSERT INTO oauth_client_details (client_id, client_secret, web_server_redirect_uri, scope, access_token_validity, refresh_token_validity, resource_ids, authorized_grant_types, additional_information) VALUES ('appointment', '{bcrypt}$2a$10$iOTcCrXSsh.CCv.KrC0ubewc7Ma3la1okv3rnP1I800e8rDs0PZIm', 'http://localhost:8080/code', 'READ,WRITE', '3600', '10000', 'doctor,patient,admission', 'authorization_code,password,refresh_token,implicit', '{}');
+
+ INSERT INTO PERMISSION (NAME) VALUES
+ ('create'),
+ ('read'),
+ ('update'),
+ ('delete');
+
+ INSERT INTO role (NAME) VALUES
+		('ROLE_ADMIN'),('ROLE_OPERATOR');
+
+/* password - bappy */
+insert into user (id, username,password, email, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked) VALUES ('1', 'sohan','{bcrypt}$2a$10$iOTcCrXSsh.CCv.KrC0ubewc7Ma3la1okv3rnP1I800e8rDs0PZIm', 'sohan@gmail.com', '1', '1', '1', '1');
+ insert into  user (id, username,password, email, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked) VALUES ('2', 'rayhan', '{bcrypt}$2a$10$iOTcCrXSsh.CCv.KrC0ubewc7Ma3la1okv3rnP1I800e8rDs0PZIm','rayhan@gmail.com', '1', '1', '1', '1');
+
+INSERT INTO ROLE_USER (ROLE_ID, USER_ID)
+    VALUES
+    (1, 1) /* sohan-admin */,
+    (2, 2) /* rayhan-operator */ ;
+
+ INSERT INTO PERMISSION_ROLE (PERMISSION_ID, ROLE_ID) VALUES
+     (1,1), /*create-> admin */
+     (2,1), /* read admin */
+     (3,1), /* update admin */
+     (4,1), /* delete admin */
+     (2,2),  /* read operator */
+     (3,2);  /* update operator */
