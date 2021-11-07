9fb26edc3d0e5a3f74ca67685a9b879c171e9777
SohanBappy <sohanbappy@gmail.com>
2021-11-08 04:41:58 +0600

    Declared as a Resource-Server

---
+++ patient/README.md
@@ -0,0 +1,3 @@
+### Declare as a Resource-server
+* token-info-uri (target authorization server, also USE client-id,secret)
+* Suppose- http://localhost:8082/patient/all (Header->Authorization[bearer {token}])
--- patient/src/main/java/com/patient/controller/HomeController.java
+++ patient/src/main/java/com/patient/controller/HomeController.java
@@ -1,17 +1,30 @@
 package com.patient.controller;
 
+import com.patient.entity.Patient;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/patient")
 @RefreshScope
 @EnableAutoConfiguration
 public class HomeController {
+
     @RequestMapping("/home")
-    public String home(){
+    public String home() {
         return "Hello from Patient!!";
     }
+
+    @RequestMapping("/all")
+    public List<Patient> allPatients() {
+        return Stream.of(new Patient(1, "sohan", "01766"),
+                new Patient(2, "rayhan", "01866")).collect(Collectors.toList());
+    }
+
 }
