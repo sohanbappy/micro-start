281758f398c33c7e8762c146ba14ef1cfef3c134
SohanBappy <sohanbappy@gmail.com>
2021-11-16 23:38:20 +0600

    Configured WebSecurityAdapter for UI access control

--- appointment/src/main/java/com/micro/appointment/controller/HomeController.java
+++ appointment/src/main/java/com/micro/appointment/controller/HomeController.java
@@ -1,11 +1,9 @@
 package com.micro.appointment.controller;
 
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@EnableOAuth2Sso
 public class HomeController {
 
     @RequestMapping(path = "/")
