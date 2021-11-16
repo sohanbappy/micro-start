f63b69a22b52ea3a06a1d703999904ccd407852c
SohanBappy <sohanbappy@gmail.com>
2021-11-16 14:02:41 +0600

    Enabled Method Level Security using OAuth2

--- patient/src/main/java/com/patient/PatientApplication.java
+++ patient/src/main/java/com/patient/PatientApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class PatientApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
